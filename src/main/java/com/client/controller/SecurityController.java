888dc456fb81001a4e989f3f9b7a04e23fbda3aa
Amlan Alok <amlanalok@gmail.com>
2023-07-10 04:44:13 -0500

    basic auth implemented

---
+++ src/main/java/com/client/controller/SecurityController.java
@@ -0,0 +1,21 @@
+package com.client.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/greetings")
+public class SecurityController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello from the API");
+    }
+
+    @GetMapping("/say-good-bye")
+    public ResponseEntity<String> sayGoodBye(){
+        return ResponseEntity.ok("Good by and see you later");
+    }
+}
