9e65e73cad226b2c69a6bafe35cd5e31361e3768
Amlan Alok <amlanalok@gmail.com>
2023-07-14 04:23:31 -0500

    commented SecurityFilterChain logic

--- src/main/java/com/client/config/SecurityConfig.java
+++ src/main/java/com/client/config/SecurityConfig.java
@@ -1,25 +1,25 @@
-package com.client.config;
-
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-/** Tells spring that this is a security configuration class */
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-// This order signifies the lowest order and hence removing this will make this func the highest order
-//    @Order(SecurityProperties.BASIC_AUTH_ORDER)
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
-        return http.build();
-    }
-}
+//package com.client.config;
+//
+//import org.springframework.boot.autoconfigure.security.SecurityProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+///** Tells spring that this is a security configuration class */
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//// This order signifies the lowest order and hence removing this will make this func the highest order
+////    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .httpBasic();
+//        return http.build();
+//    }
+//}
